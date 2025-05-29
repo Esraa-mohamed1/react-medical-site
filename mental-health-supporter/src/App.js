e9df88ce21008feb7b4ab6399c1ee52729107451
fatmamustafamuhammed <fmomen738@gmail.com>
2025-05-29 18:04:06 +0300

    Doctors List Page is Done Successfully

--- mental-health-supporter/src/App.js
+++ mental-health-supporter/src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from "./pages/RegisterPage"
-import ArticalPage from "./pages/ArticalPage"
- 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ArticalPage from "./pages/ArticalPage";
+import DoctorsList from './components/DoctorsList/DoctorsList'; 
+
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/artical" element={<ArticalPage />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/artical" element={<ArticalPage />} />
+        <Route path="/doctors" element={<DoctorsList />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
