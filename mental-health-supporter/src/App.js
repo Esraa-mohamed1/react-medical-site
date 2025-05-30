6b69e7e7f26b2899585aaa8bf78963dc938db226
ayaAhmed119 <sultantotta@gmail.com>
2025-05-30 11:50:38 +0300

    view Details Page and booking

--- mental-health-supporter/src/App.js
+++ mental-health-supporter/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DoctorPage from './pages/DoctorPage';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from "./pages/RegisterPage"
-import ArticalPage from "./pages/ArticalPage"
- 
+
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/artical" element={<ArticalPage />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-  </Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/doctor/:id" element={<DoctorPage />} />
+          <Route path="/" element={<DoctorPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
