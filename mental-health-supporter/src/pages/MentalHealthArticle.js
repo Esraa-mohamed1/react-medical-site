546f034614c136c67487c5c80a0c060ed2ef3c0a
Alshimaa Shahat <alshimaashahat21@gmail.com>
2025-05-30 11:24:02 +0300

    decrease higt of header

--- mental-health-supporter/src/pages/MentalHealthArticle.js
+++ mental-health-supporter/src/pages/MentalHealthArticle.js
@@ -33,7 +33,7 @@ const MentalHealthArticle = ({ clientData = {} }) => {
       <header className="article-header">
         <div className="header-overlay"></div>
         <h1>MENTAL HEALTH SUPPORT</h1>
-        <h2>Personalized Emotional Management Guide</h2>
+        {/* <h2>Personalized Emotional Management Guide</h2> */}
         <div className="user-greeting">
           <span className="welcome">Welcome, </span>
           <span className="username">{name}</span>
