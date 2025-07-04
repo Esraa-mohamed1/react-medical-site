d8867057756008998c850c4bdcaa866446a29a8b
fatmamustafamuhammed <fmomen738@gmail.com>
2025-07-04 14:52:25 +0300

    fix footer in article page

--- mental-health-supporter/src/pages/ArticlePage.js
+++ mental-health-supporter/src/pages/ArticlePage.js
@@ -39,8 +39,9 @@ export default function ArticlePage() {
           <ProfessionalHelp disorder={disorderType} />
         </main>
 
-        <Footer />
       </div>
+              <Footer />
+
     </>
   );
 }
